6f6f02174483bf9d133fd3c30370336242c02552
Beren Minor <beren.minor+git@gmail.com>
2015-04-21 11:16:09 +0200

    Add module map file.

--- include/boost/typeof/int_encoding.hpp
+++ include/boost/typeof/int_encoding.hpp
@@ -54,14 +54,14 @@ namespace boost { namespace type_of {
     ////////////////////////////////
 
     template<class V, std::size_t n, bool overflow = (n >= 0x3fffffff)>
-    struct encode_size_t : push_back<
+    struct encode_size_t : mpl::push_back<
         V,
         boost::mpl::size_t<pack<n, false>::value>
     >
     {};
 
     template<class V, std::size_t n>
-    struct encode_size_t<V, n, true> : push_back<typename push_back<
+    struct encode_size_t<V, n, true> : mpl::push_back<typename mpl::push_back<
         V,
         boost::mpl::size_t<pack<n % 0x3ffffffe, true>::value> >::type,
         boost::mpl::size_t<n / 0x3ffffffe>
